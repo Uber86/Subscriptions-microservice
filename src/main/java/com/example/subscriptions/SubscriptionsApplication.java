e12839a08790259b10abad5a4fd18ef48955fdb6
Сергей <lockup86@rambler.ru>
2025-06-06 21:13:25 +0300

    1

--- src/main/java/com/example/subscriptions/SubscriptionsApplication.java
+++ src/main/java/com/example/subscriptions/SubscriptionsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SubscriptionsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionsApplication.class, args);
 	}
